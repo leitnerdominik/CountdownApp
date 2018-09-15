9b0f76532dba1d00a25e509ab54009c2139c1f4d
Dominik <leitner.dominik@hotmail.de>
2018-09-15 23:52:11 +0200

    fixed dropdown in settings

--- src/components/UI/Dropdown/Dropdown.js
+++ src/components/UI/Dropdown/Dropdown.js
@@ -3,14 +3,16 @@ import React from 'react';
 import './Dropdown.css';
 
 const dropDown = props => (
-    <div className="SelectContainer">
-        <select className="SelectDropDown">
-        {props.options.map((option, index) => {
-            return (
-                <option value={index + 1} key={index}>{option}</option>
-            );
-        })}
-        </select>
+    <div style={{position: 'relative'}}>
+        <div className="SelectContainer">
+            <select className="SelectDropDown">
+            {props.options.map((option, index) => {
+                return (
+                    <option value={index + 1} key={index}>{option}</option>
+                );
+            })}
+            </select>
+        </div>
     </div>
 );
 
